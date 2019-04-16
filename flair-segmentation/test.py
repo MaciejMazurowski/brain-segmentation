bbd8c96351c2c4c691455c75b05bc03a1e16c8bc
Mateusz Buda <mateusz.buda@duke.edu>
2019-04-16 14:38:19 -0400

    fixed formatting

--- flair-segmentation/test.py
+++ flair-segmentation/test.py
@@ -1,7 +1,8 @@
 from __future__ import print_function
 
 import matplotlib
-matplotlib.use('Agg')
+
+matplotlib.use("Agg")
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
@@ -9,7 +10,8 @@ import os
 import sys
 import tensorflow as tf
 import warnings
-warnings.filterwarnings('ignore')
+
+warnings.filterwarnings("ignore")
 
 from keras import backend as K
 from scipy.io import savemat
@@ -18,12 +20,12 @@ from skimage.io import imsave
 from data import load_data
 from net import unet
 
-weights_path = './weights_128.h5'
-train_images_path = './data/train/'
-test_images_path = './data/valid/'
-predictions_path = './predictions/'
+weights_path = "./weights_128.h5"
+train_images_path = "./data/train/"
+test_images_path = "./data/valid/"
+predictions_path = "./predictions/"
 
-gpu = '0'
+gpu = "0"
 
 
 def predict(mean=20.0, std=43.0):
@@ -51,12 +53,12 @@ def predict(mean=20.0, std=43.0):
         os.mkdir(predictions_path)
 
     matdict = {
-        'pred': imgs_mask_pred,
-        'image': original_imgs_test,
-        'mask': imgs_mask_test,
-        'name': names_test
+        "pred": imgs_mask_pred,
+        "image": original_imgs_test,
+        "mask": imgs_mask_test,
+        "name": names_test,
     }
-    savemat(os.path.join(predictions_path, 'predictions.mat'), matdict)
+    savemat(os.path.join(predictions_path, "predictions.mat"), matdict)
 
     # save images with segmentation and ground truth mask overlay
     for i in range(len(imgs_test)):
@@ -70,14 +72,16 @@ def predict(mean=20.0, std=43.0):
         # prediction contour image
         pred = (np.round(pred[:, :, 0]) * 255.0).astype(np.uint8)
         pred, contours, _ = cv2.findContours(
-            pred, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+            pred, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE
+        )
         pred = np.zeros(pred.shape)
         cv2.drawContours(pred, contours, -1, (255, 0, 0), 1)
 
         # ground truth contour image
         mask = (np.round(mask[:, :, 0]) * 255.0).astype(np.uint8)
         mask, contours, _ = cv2.findContours(
-            mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+            mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE
+        )
         mask = np.zeros(mask.shape)
         cv2.drawContours(mask, contours, -1, (255, 0, 0), 1)
 
@@ -89,8 +93,7 @@ def predict(mean=20.0, std=43.0):
         pred_rgb[:, :, 2] = np.maximum(pred_rgb[:, :, 2], mask)
         pred_rgb[:, :, 0] = np.maximum(pred_rgb[:, :, 0], pred)
 
-        imsave(os.path.join(predictions_path,
-                            names_test[i] + '.png'), pred_rgb)
+        imsave(os.path.join(predictions_path, names_test[i] + ".png"), pred_rgb)
 
     return imgs_mask_test, imgs_mask_pred, names_test
 
@@ -106,20 +109,22 @@ def evaluate(imgs_mask_test, imgs_mask_pred, names_test):
     i = 0  # start slice index
     for p in range(len(name_test_pred)):
         # get case id (names are in format <case_id>_<slice_number>)
-        p_id = '_'.join(name_test_pred[p][0].split('_')[:-1])
+        p_id = "_".join(name_test_pred[p][0].split("_")[:-1])
 
         # if this is the last slice for the processed case
         if p + 1 >= len(name_test_pred) or p_id not in name_test_pred[p + 1][0]:
             # ground truth segmentation:
             p_slices_mask = np.array(
-                [im_m[0] for im_id, im_m in name_test_pred[i:p + 1]])
+                [im_m[0] for im_id, im_m in name_test_pred[i : p + 1]]
+            )
             # predicted segmentation:
             p_slices_pred = np.array(
-                [im_m[1] for im_id, im_m in name_test_pred[i:p + 1]])
+                [im_m[1] for im_id, im_m in name_test_pred[i : p + 1]]
+            )
 
             patient_ids.append(p_id)
             dc_values.append(dice_coefficient(p_slices_pred, p_slices_mask))
-            print(p_id + ':\t' + str(dc_values[-1]))
+            print(p_id + ":\t" + str(dc_values[-1]))
 
             i = p + 1
 
@@ -129,7 +134,11 @@ def evaluate(imgs_mask_test, imgs_mask_pred, names_test):
 def dice_coefficient(prediction, ground_truth):
     prediction = np.round(prediction).astype(int)
     ground_truth = np.round(ground_truth).astype(int)
-    return np.sum(prediction[ground_truth == 1]) * 2.0 / (np.sum(prediction) + np.sum(ground_truth))
+    return (
+        np.sum(prediction[ground_truth == 1])
+        * 2.0
+        / (np.sum(prediction) + np.sum(ground_truth))
+    )
 
 
 def gray2rgb(im):
@@ -143,21 +152,21 @@ def plot_dc(labels, values):
     y_pos = np.arange(len(labels))
 
     fig = plt.figure(figsize=(12, 8))
-    plt.barh(y_pos, values, align='center', alpha=0.5)
+    plt.barh(y_pos, values, align="center", alpha=0.5)
     plt.yticks(y_pos, labels)
     plt.xticks(np.arange(0.5, 1.0, 0.05))
-    plt.xlabel('Dice coefficient', fontsize='x-large')
-    plt.axes().xaxis.grid(color='black', linestyle='-', linewidth=0.5)
+    plt.xlabel("Dice coefficient", fontsize="x-large")
+    plt.axes().xaxis.grid(color="black", linestyle="-", linewidth=0.5)
     axes = plt.gca()
     axes.set_xlim([0.5, 1.0])
     plt.tight_layout()
-    axes.axvline(np.mean(values), color='green', linewidth=2)
+    axes.axvline(np.mean(values), color="green", linewidth=2)
 
-    plt.savefig('DSC.png', bbox_inches='tight')
+    plt.savefig("DSC.png", bbox_inches="tight")
     plt.close(fig)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
@@ -166,13 +175,13 @@ if __name__ == '__main__':
 
     if len(sys.argv) > 1:
         gpu = sys.argv[1]
-    device = '/gpu:' + gpu
+    device = "/gpu:" + gpu
 
     with tf.device(device):
         imgs_mask_test, imgs_mask_pred, names_test = predict()
         values, labels = evaluate(imgs_mask_test, imgs_mask_pred, names_test)
 
-    print('\nAverage DSC: ' + str(np.mean(values)))
+    print("\nAverage DSC: " + str(np.mean(values)))
 
     # plot results
     plot_dc(labels, values)
--- flair-segmentation/train.py
+++ flair-segmentation/train.py
@@ -1,12 +1,11 @@
 from __future__ import print_function
 
-import numpy as np
 import os
 import sys
-import tensorflow as tf
 
+import numpy as np
+import tensorflow as tf
 from keras import backend as K
-from keras.callbacks import ModelCheckpoint
 from keras.callbacks import TensorBoard
 from keras.optimizers import Adam
 
@@ -16,13 +15,13 @@ from net import dice_coef
 from net import dice_coef_loss
 from net import unet
 
-train_images_path = './data/train/'
-valid_images_path = './data/valid/'
-init_weights_path = './weights_128.h5'
-weights_path = '.'
-log_path = '.'
+train_images_path = "./data/train/"
+valid_images_path = "./data/valid/"
+init_weights_path = "./weights_128.h5"
+weights_path = "."
+log_path = "."
 
-gpu = '0'
+gpu = "0"
 
 epochs = 128
 batch_size = 32
@@ -50,29 +49,29 @@ def train():
         model.load_weights(init_weights_path)
 
     optimizer = Adam(lr=base_lr)
-    model.compile(optimizer=optimizer,
-                  loss=dice_coef_loss,
-                  metrics=[dice_coef])
+    model.compile(optimizer=optimizer, loss=dice_coef_loss, metrics=[dice_coef])
 
     if not os.path.exists(log_path):
         os.mkdir(log_path)
 
     training_log = TensorBoard(log_dir=log_path)
 
-    model.fit(imgs_train, imgs_mask_train,
-              validation_data=(imgs_valid, imgs_mask_valid),
-              batch_size=batch_size,
-              epochs=epochs,
-              shuffle=True,
-              callbacks=[training_log])
+    model.fit(
+        imgs_train,
+        imgs_mask_train,
+        validation_data=(imgs_valid, imgs_mask_valid),
+        batch_size=batch_size,
+        epochs=epochs,
+        shuffle=True,
+        callbacks=[training_log],
+    )
 
     if not os.path.exists(weights_path):
         os.mkdir(weights_path)
-    model.save_weights(os.path.join(
-        weights_path, 'weights_{}.h5'.format(epochs)))
+    model.save_weights(os.path.join(weights_path, "weights_{}.h5".format(epochs)))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
     config.allow_soft_placement = True
@@ -81,7 +80,7 @@ if __name__ == '__main__':
 
     if len(sys.argv) > 1:
         gpu = sys.argv[1]
-    device = '/gpu:' + gpu
+    device = "/gpu:" + gpu
 
     with tf.device(device):
         train()
